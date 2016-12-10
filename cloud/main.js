9469cb11ddcfd527f8a09fecd0b1aff8b58996ae
Felix Honold <fh@innovate-it.ch>
2016-12-10 20:44:49 +0100

    date trial

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,6 @@
 require("./functions/hello");
-// require("./functions/resetPlayerNotes");
+require("./functions/hello1");
+require("./functions/resetPlayerNotes");
 
 // require("./triggers/testObject");
 // require("./triggers/player");
