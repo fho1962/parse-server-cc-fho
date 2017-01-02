57c6a243e76332a072f192204adf6f66a4b4736a
Felix Honold <fh@innovate-it.ch>
2017-01-02 09:58:17 +0100

    Version 0.4

--- cloud/main.js
+++ cloud/main.js
@@ -1,9 +1,13 @@
-require("./functions/hello");
+//require("./functions/hello");
 require("./functions/hello1");
 require("./functions/hello2");
 require("./functions/resetPlayerNotes");
 
 // require("./triggers/testObject");
 // require("./triggers/player");
+
+Parse.Cloud.define('hello', function(req, res) {
+  res.success('hello world');
+});
 
 
